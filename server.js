80e8237820344663bca348d27db79f2155486f24
Cosimoyannis <Cosimo.t@live.de>
2022-07-09 00:31:39 +0200

    vue

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("your app is listening on port" + listener.adress().port);
-})
